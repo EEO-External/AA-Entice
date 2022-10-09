2b6602aa4426578ee514f8996142ba8dcc1ebb0c
Kenneth Rice <kenneth1.rice1@gmail.com>
2022-10-09 00:44:10 -0500

    connecting database

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,33 @@
-from flask import Flask
-
+from urllib import request, response
+from flask import Flask, render_template, session, request, redirect, url_for, jsonify
+from flask_pymongo import PyMongo
+from models import *
 app = Flask(__name__)
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://kennethri:T2elJXbvg85FayiU@cluster0.fqadzuj.mongodb.net/?retryWrites=true&w=majority")
+users = db = client.database.users
 
-@app.route('/members')
-def members():
-    return {"members": ["Member1", "Member2"]}
+
+sample = {
+    "username": "kennyice",
+    "password": "1234",
+    "status": "active",
+    "points": 1200,
+    "goal": 5000,
+    "referee": "abc123",
+    "referal": "cba124"
+}
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        #users.insert_one(sample)
+        response = jsonify({"some":"blahhh"})
+    else: response = jsonify({"None":"None"})
     
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 if __name__ == "__main__":
     app.run(debug=True)
