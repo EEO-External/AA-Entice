148609e4acf68e7dc50ac135fd7f3120f549cbb5
Philben Pierre <82547190+phillycodes@users.noreply.github.com>
2022-10-09 02:51:46 -0500

    modified files

--- referral-client/src/App.js
+++ referral-client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import Navbar from 'Navbar.js'
+import Navbar from './Navbar.js'
+
+
 
 function App() {
 
--- referral-client/src/Navbar.js
+++ referral-client/src/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {BiSearch} from 'react-icons/bi'
 import {BsPerson} from 'react-icons/bs'
+import {HiOutlineMenuAlt4} from 'react-icons/hi'
+import './index.css'
+import './NavbarStyles.css'
 
 function Navbar() {
   return (
@@ -17,6 +20,10 @@ function Navbar() {
             <BiSearch className='icon' />
             <BsPerson className="icon" />
         </div>
+        <div className="hamburger">
+          <HiOutlineMenuAlt4 className='icon' />
+        </div>
+      
     </div>
   )
 }
